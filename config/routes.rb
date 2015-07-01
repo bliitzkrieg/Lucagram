08a1830a5866797bab7d79468684f9edd3ceb859
Luca D <lucamatthewdecaprio@gmail.com>
2015-06-30 20:59:32 -0400

    adding post controllers/models

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  root 'application#hello'
-
+  root 'posts#index'
+  resources :posts
+    
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
