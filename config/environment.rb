d9765a15e4b110ac1c71c28ba7ebe451719a5265
Luca D <lucamatthewdecaprio@gmail.com>
2015-06-30 21:07:59 -0400

    added image processing library

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Initiate paperclip
+Paperclip.options[:command_path] = "/usr/bin/convert"
